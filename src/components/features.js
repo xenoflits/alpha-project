625753fc0a07913a5d3ca81f61202265605f597f
unknown <soeradj.mahabiersing@gmail.com>
2018-02-13 01:45:42 +0100

    initial components

---
+++ src/components/features.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class Features extends React.Component {
+    render() {
+        return (
+            <article>{this.props.feature}</article>
+        )
+    }
+}
---
+++ src/components/navigation.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class Navigation extends React.Component {
+    render() {
+        return (<nav className="navigation">
+            <li>home</li>
+            <li>features</li>
+            <li>contact</li>
+        </nav>)
+    }
+}
